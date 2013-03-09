dfa2113df9e31edf5b3fdceabbd02197bb9c3c76
László Monda <laci@monda.hu>
2013-03-09 01:03:21 +0100

    Use the config file instead of a hardcoded path.

--- update-latest-sms.sh
+++ update-latest-sms.sh
@@ -2,7 +2,7 @@
 
 # Copy the most up-to-date SMS backup as $destination_sms
 # to a directory where your webserver can access it.
-destination_sms=~/projects/sms/sms.xml
+. `dirname $0`/config.sh  # defines the $destination_sms variable
 
 cd ~/Dropbox/Apps/SMSBackupRestore
 last_sms=`ls -1 sms-????-??-??_??-??-??.xml | tail --lines=1`
